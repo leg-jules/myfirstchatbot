1f0c0b497c2116a8ff7bc6b7fbb156946b280d66
leg-jules <jules.legalais@efrei.net>
2023-11-25 19:50:13 +0100

    fonctions

--- functions.py
+++ functions.py
@@ -1,6 +1,7 @@
 import os
 import re
-
+import math
+from collections import Counter
 
 def extract_president_name(filename):
     # Extrait le nom du président du nom du fichier
@@ -64,4 +65,44 @@ def tf(text):
 
     return tf_dict
 
-def idf():
+def idf(corpus_directory):
+    # Dictionnaire pour stocker le nombre de documents dans lesquels chaque mot apparaît
+    doc_count = Counter()
+
+    # Compter le nombre de documents dans lesquels chaque mot apparaît
+    for filename in os.listdir(corpus_directory):
+        with open(os.path.join(corpus_directory, filename), 'r', encoding='utf-8') as file:
+            words = file.read().split()
+            # Utiliser un ensemble pour s'assurer que chaque document contribue une seule fois par mot
+            unique_words = set(words)
+            doc_count.update(unique_words)
+
+    # Nombre total de documents dans le corpus
+    total_docs = len(os.listdir(corpus_directory))
+
+    # Calcul de l'IDF pour chaque mot
+    idf_dict = {word: math.log(total_docs / (count + 1)) for word, count in doc_count.items()}
+
+    return idf_dict
+
+def tfidf_matrix(corpus_directory, tf, idf):
+    # Liste pour stocker les vecteurs IDF pour chaque mot
+    idf_vector = idf([document.split() for document in os.listdir(corpus_directory)])
+
+    # Créer la matrice TF-IDF
+    matrix = []
+    words = []
+    for filename in os.listdir(corpus_directory):
+        with open(os.path.join(corpus_directory, filename), 'r', encoding='utf-8') as file:
+            document = file.read().split()
+            # Calculer le vecteur TF pour chaque document
+            tf_vector = tf(document)
+            # Multiplication par le vecteur IDF pour obtenir le vecteur TF-IDF
+            tfidf_vector = {word: tf * idf_vector[word] for word, tf in tf_vector.items()}
+            matrix.append(list(tfidf_vector.values()))
+            words.extend(tfidf_vector.keys())
+
+def mot_peu_important(tfidf_matrix, words):
+    peu_important = [word for word in words if all(tfidf_matrix[words.index(word)]) == 0]
+    return peu_important
+

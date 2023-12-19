bc8c11d4a98053992b981d7c09235e3bfada86d3
SteedEfrei <150119067+SteedEfrei@users.noreply.github.com>
2023-12-19 08:45:10 +0100

    Update functions.py

--- functions.py
+++ functions.py
@@ -44,19 +44,18 @@ def lower(text):  # Convertit le texte en minuscules
     return text2
 
 
-def new_dscr(disc_process, output_file):
+def new_dscr(repertory):
     # lire le fichier
-    for filename in os.listdir(disc_process):
-        with open(disc_process + "/" + filename, "r", encoding="utf-8") as f:
-            dscr = f.read()
-        # traiter le texte
-        cleaned_dscr = re.sub(r"[^\w\s]", "", lower(dscr))
+    for filename in os.listdir(repertory):
+        with open(repertory+"/"+filename, "r", encoding="utf-8") as f, open("cleaned/"+filename, "w", encoding="utf-8") as cleaned_f:
+            # traiter le texte
+            for car in f.read():
+                for k in ".:;,!/?'-":
+                    car = car.lower().replace(k, " ")
+                cleaned_f.write(car)
 
-        with open(output_file + "/" + filename, "w", encoding="utf-8") as cleaned_f:
-            cleaned_f.write(cleaned_dscr)
-#enregisrte dans le bon fichier
 
-def tf(text):
+def term_frequency(text):
     words = text.split()
     tf_dict = {}
 
@@ -65,154 +64,125 @@ def tf(text):
 
     return tf_dict
 
-def idf(corpus_directory):
-    # Créer un dictionnaire pour stocker le nombre de documents dans lesquels chaque mot apparaît
-    doc_count = {}
 
-    # Parcourir tous les documents du corpus
+def idf(corpus_directory):
+    # Dictionnaire pour stocker le nombre de documents dans lesquels chaque mot apparaît
+    doc_count = Counter()
+
+    # Compter le nombre de documents dans lesquels chaque mot apparaît
     for filename in os.listdir(corpus_directory):
         with open(os.path.join(corpus_directory, filename), 'r', encoding='utf-8') as file:
             words = file.read().split()
+            # Utiliser un ensemble pour s'assurer que chaque document contribue une seule fois par mot
+            unique_words = set(words)
+            doc_count.update(unique_words)
 
-            # Mettre à jour le dictionnaire `doc_count` avec le nombre de documents pour chaque mot
-            for word in words:
-                if word not in doc_count:
-                    doc_count[word] = 0
-                doc_count[word] += 1
+    # Nombre total de documents dans le corpus
+    total_docs = len(os.listdir(corpus_directory))
 
-    # Calculer l'IDF pour chaque mot
-    idf_dict = {word: math.log(len(os.listdir(corpus_directory)) / count) for word, count in doc_count.items()}
+    # Calcul de l'IDF pour chaque mot
+    idf_dict = {word: math.log10(total_docs / (count)) for word, count in doc_count.items()}
 
     return idf_dict
 
-def tfidf_matrix(corpus_directory, tf, idf):
-    # Create a dictionary to store IDF values for each word
-    idf_dict = {}
-    for document in os.listdir(corpus_directory):
-        document_words = document.split()
-        # Calculate IDF for each word in the corpus
-        for word in set(document_words):
-            idf_dict[word] = idf(document_words)
 
-    # Create a TF-IDF matrix
-    tfidf_matrix = []
-    word_list = []
+def tfidf_matrix(corpus_directory):
+    # Liste pour stocker les vecteurs IDF pour chaque mot
+    idf_vector = idf(corpus_directory)
+
+    # Créer la matrice TF-IDF
+    matrix = []
+    words = []
     for filename in os.listdir(corpus_directory):
+        dico = {}
         with open(os.path.join(corpus_directory, filename), 'r', encoding='utf-8') as file:
-            document = file.read().split()
+            document = file.read()
+            # Calculer le vecteur TF pour chaque document
+            tf_vector = term_frequency(document)
+            # Multiplication par le vecteur IDF pour obtenir le vecteur TF-IDF
+            matrix.append(filename)
+            for word, tf in idf_vector.items():
+                for cle, val in tf_vector.items():
+                    if str(cle) == str(word):
+                        dico[word]=round(tf * val,3)
+                if word not in tf_vector.keys():
+                        dico[word] = 0.0
+        matrix.append(dico)
+    return matrix
 
-            # Calculate TF vector for each document
-            tf_vector = tf(document)
+def additionDico(directory):
+    dicoAdd = {}
+    matrice = tfidf_matrix(directory)
+    for i in range(1,16,2):
+        for cle, val in matrice[i].items():
+            if cle not in dicoAdd.keys():
+                dicoAdd[cle] = val
+            else:
+                dicoAdd[cle] += val
+    return dicoAdd
 
-            # Calculate TF-IDF vector by multiplying TF with IDF values
-            tfidf_vector = {
-                word: tf * idf_dict[word]
-                for word, tf in tf_vector.items()
-            }
-            tfidf_matrix.append(list(tfidf_vector.values()))
+def mot_peu_important(directory):
+    peu_important = []
+    matrice = additionDico(directory)
+    for mots, valeurs in matrice.items():
+        if valeurs == 0.0:
+            peu_important.append(mots)
+    return peu_important
 
-            # Append unique words to the word list
-            word_list.extend(tfidf_vector.keys())
+def highest_tfidf_words(directory):
+    matrice = tfidf_matrix(directory)
+    score = 0
+    mot = ""
+    for i in range(1,16,2):
+        for cle, val in matrice[i].items():
+            if score<val:
+                score = val
+                mot = cle
+            elif score == val:
+                mot = mot+cle
+    return mot
 
-    return tfidf_matrix, word_list
+def most_repeated_words_by_president(files:list):
+    for element in files:
+        text = open("cleaned/"+element,'r', encoding='utf-8').read()
+        score = term_frequency(text)
+        for cle, val in score.items():
+            if cle not in score.keys():
+                score[cle] = val
+            else:
+                score[cle] += val
+        max_score = max(score, key=score.get)
+    return max_score
 
+def president_mentions_of_nation(directory,mot):
+    dico = {}
+    for filename in os.listdir(directory):
+        cpt = 0
+        with open(os.path.join(directory, filename), 'r', encoding='utf-8') as file:
+            document = file.read().lower().split()
+            for i in range(len(document)):
+                if str(document[i]) == str(mot.lower()):
+                    cpt += 1
+        if cpt != 0:
+            dico[filename] = cpt
+    return dico, max(dico, key=dico.get)
 
-def mot_peu_important(tfidf_matrix, words):
-    peu_important = [word for word in words if all(tfidf_matrix[words.index(word)]) == 0]
-    return peu_important               # retourne les mots les moins importants
+def first_president_to_mention_climate_ecology(directory,mot):
+    dico = {}
+    for filename in os.listdir(directory):
 
-def highest_tfidf_words(tfidf_matrix, words, n=1):
-    # Extract the highest TF-IDF scores
-    max_tfidf = tfidf_matrix.max(axis=1)
+        i = 0
+        with open(os.path.join(directory, filename), 'r', encoding='utf-8') as file:
+            document = file.read().lower().split()
+            for i in range(len(document)):
+                if str(document[i]) == str(mot.lower()):
+                    dico[filename] = i
+    if len(dico)!= 0:
+        return min(dico, key=dico.get)
+    else:
+        return "Pas trouvé"
 
-    # Find the indices of the highest TF-IDF scores
-    highest_tfidf_indices = max_tfidf.argsort()[-n:][::-1]
-
-    # Return the corresponding words
-    return [words[i] for i in highest_tfidf_indices]
-def most_repeated_words_by_president(tfidf_matrix, words, president_name):
-    president_indices = [i for i, word in enumerate(words) if president_name.lower() in word.lower()]
-    president_tfidf_sum = [sum(tfidf_matrix[:, idx]) for idx in president_indices]
-    most_repeated_index = president_indices[president_tfidf_sum.index(max(president_tfidf_sum))]
-    return words[most_repeated_index]
-
-def president_mentions_of_nation(tfidf_matrix, words):
-    nation_indices = [i for i, word in enumerate(words) if 'nation' in word.lower()]
-    president_nation_counts = [sum(tfidf_matrix[:, idx] > 0) for idx in nation_indices]
-    most_mentions_index = nation_indices[president_nation_counts.index(max(president_nation_counts))]
-    return words[most_mentions_index]
-
-def first_president_to_mention_climate_ecology(tfidf_matrix, words):
-    climate_ecology_indices = [i for i, word in enumerate(words) if 'climate' in word.lower() or 'ecology' in word.lower()]
-    first_president_index = min([next(i for i, value in enumerate(tfidf_matrix[:, idx] > 0) if value) for idx in climate_ecology_indices])
-    return words[first_president_index]
 
 def common_words_among_presidents(tfidf_matrix, words):
     common_words_indices = [i for i in range(len(words)) if all(tfidf_matrix[:, i] > 0)]
     return [words[i] for i in common_words_indices]
-
-
-def load_text_from_file(file_path):
-    with open(file_path, 'r', encoding='utf-8') as file:
-        return file.read()
-
-
-def get_tfidf_for_word(word, tfidf_matrix, words, directory):
-    word_index = words.index(word)
-    idf = calculate_idf(word, directory)
-
-    tfidf_values = [row[word_index] * idf for row in tfidf_matrix]
-    return tfidf_values
-
-
-def calculate_idf(word, directory):
-    document_count = len(list_of_files(directory, "txt"))
-    documents_with_word = sum(
-        1 for filename in list_of_files(directory, "txt") if word_in_document(word, os.path.join(directory, filename)))
-
-    return math.log(document_count / (1 + documents_with_word))
-
-
-def word_in_document(word, document_path):
-    with open(document_path, 'r', encoding="utf-8") as file:
-        document = file.read()
-    return word in document
-
-
-def list_least_important_words(tfidf_matrix, words, directory):
-    least_important_words = []
-
-    for word in words:
-        tfidf_values = get_tfidf_for_word(word, tfidf_matrix, words, directory)
-        if all(value == 0 for value in tfidf_values):
-            least_important_words.append(word)
-
-    return least_important_words
-
-
-def list_most_important_words(tfidf_matrix, words):
-    max_tfidf_values = [max(row) for row in tfidf_matrix]
-    max_tfidf_index = max_tfidf_values.index(max(max_tfidf_values))
-
-    most_important_words = [words[i] for i, value in enumerate(tfidf_matrix[max_tfidf_index]) if
-                            value == max_tfidf_values[max_tfidf_index]]
-
-    return most_important_words
-
-
-def most_repeated_words_by_president(president, words, tfidf_matrix, directory):
-    president_files = [filename for filename in list_of_files(directory, "txt") if
-                       president.lower() in filename.lower()]
-    if not president_files:
-        return None
-
-    president_text = " ".join([load_text_from_file(os.path.join(directory, filename)) for filename in president_files])
-    cleaned_president_text = re.sub(r"[^\w\s]", "", lower(president_text))
-
-    president_words = cleaned_president_text.split()
-    tfidf_values = {word: get_tfidf_for_word(word, tfidf_matrix, words, directory) for word in president_words}
-
-    most_repeated_words = [word for word, values in tfidf_values.items() if
-                           sum(values) == max(sum(v) for v in tfidf_values.values())]
-
-    return most_repeated_words
